bf1aef321a1521084927d2886d7680e45896081c
apir8181 <kazenoyumechen@gmail.com>
2016-06-17 18:02:06 +0800

    update doc2vec result

--- rumor_detect/document_modelling/doc2vec/run.py
+++ rumor_detect/document_modelling/doc2vec/run.py
@@ -16,44 +16,38 @@ class HelperCorpus:
     """
     Corpus helper for training doc2vec.
     """
-    def __init__(self, pos_path, neg_path):
+    def __init__(self, path, prefix):
         """
-        Construct corpus iterator from files (pos_path and neg_path).
+        Construct corpus iterator from files.
         Input:
-        - pos_path: csv file containing positive documents.
-        - neg_path: csv file containing negative documents.
+        - path: csv file containing documents.
         """
-        self.pos_corpus_ = TextSegmentedCorpus(pos_path)
-        self.neg_corpus_ = TextSegmentedCorpus(neg_path)
+        self.corpus_ = TextSegmentedCorpus(path)
+        self.prefix_ = prefix
 
     def __iter__(self):
         """
-        Iterate over positive documents and negative documents sequentially.
+        Iterate over documents sequentially.
         Output:
-        - tagged_doc: an doc2vec.TaggedDocument object. Positive documents are
-        tagged as 'POS_<iter_num>'. Negative documents are tagged as
-        'NEG_<iter_num>'.
+        - tagged_doc: an doc2vec.TaggedDocument object. Each document is tagged
+         as <prefix>_<idxnum>
         """
-        for i, sentence in enumerate(self.pos_corpus_):
-            yield TaggedDocument(sentence, ["POS_%d" % i])
-            self.pos_count_ = i
-        for i, sentence in enumerate(self.neg_corpus_):
-            yield TaggedDocument(sentence, ["NEG_%d" % i])
-            self.neg_count_ = i
+        for i, sentence in enumerate(self.corpus_):
+            yield TaggedDocument(sentence, ["%s_%d" % (self.prefix_, i)])
+            self.count_ = i + 1
 
-    def pos_count(self):
-        return self.pos_count_
-
-    def neg_count(self):
-        return self.neg_count_
+    def count(self):
+        return self.count_
 
 
 class MemoryHelperCorpus:
     def __init__(self, pos_path, neg_path):
-        helper_corpus = HelperCorpus(pos_path, neg_path)
-        self.sentences_ = [sentence for sentence in helper_corpus]
-        self.pos_count_ = helper_corpus.pos_count()
-        self.neg_count_ = helper_corpus.neg_count()
+        pos_corpus = HelperCorpus(pos_path, "POS")
+        neg_corpus = HelperCorpus(neg_path, "NEG")
+        self.sentences_ = [sentence for sentence in pos_corpus]
+        self.sentences_.extend([sentence for sentence in neg_corpus])
+        self.pos_count_ = pos_corpus.count()
+        self.neg_count_ = neg_corpus.count()
 
     def __iter__(self):
         random.shuffle(self.sentences_)
@@ -67,46 +61,36 @@ class MemoryHelperCorpus:
         return self.neg_count_
 
 
-def grid_search(pos_path, neg_path):
+def grid_search(doc_path, pos_path, neg_path):
     logger = logging.getLogger("doc2vec.run")
-    results = {}
 
-    for neg in [5, 10, 20, 40]:
-        # fix random seed to remove perturbation
-        random.seed(12345)
+    # pretrain doc from a large dataset
+    doc_corpus = HelperCorpus(doc_path, "DOC")
+    model = Doc2Vec(doc_corpus, size=100, alpha=0.025, window=4, min_count=5,
+                    sample=0, hs=0, negative=5, dm_concat=1, iter=5, workers=7)
 
-        logger.info("Feeding corpus into memory")
-        corpus = MemoryHelperCorpus(pos_path, neg_path)
 
-        logger.info("Training Doc2Vec")
-        model = Doc2Vec(corpus, size=100, alpha=0.025, window=4, min_count=5,
-                        sample=0, hs=0, negative=neg, dm_concat=1, iter=10, workers=7)
+    # fix random seed to remove perturbation
+    logger.info("Feeding corpus into memory")
+    corpus = MemoryHelperCorpus(pos_path, neg_path)
+    logger.info("Training Doc2Vec")
+    model.train(corpus)
 
-        logger.info("Retrieving Doc2Vec")
-        X = np.zeros((corpus.pos_count() + corpus.neg_count(), 100))
-        y = np.zeros(corpus.pos_count() + corpus.neg_count(), dtype=np.int32)
-        for pos_i in range(corpus.pos_count()):
-            X[pos_i] = model.docvecs['POS_%d' % pos_i]
-            y[pos_i] = 1
-        for neg_i in range(corpus.neg_count()):
-            X[corpus.pos_count() + neg_i] = model.docvecs['NEG_%d' % neg_i]
-            y[neg_i] = -1
+    logger.info("Retrieving Doc2Vec")
+    X = np.zeros((corpus.pos_count() + corpus.neg_count(), 100))
+    y = np.zeros(corpus.pos_count() + corpus.neg_count(), dtype=np.int32)
+    for pos_i in range(corpus.pos_count()):
+        X[pos_i] = model.docvecs['POS_%d' % pos_i]
+        y[pos_i] = 1
+    for neg_i in range(corpus.neg_count()):
+        X[corpus.pos_count() + neg_i] = model.docvecs['NEG_%d' % neg_i]
+        y[neg_i] = -1
 
-        logger.info("Training LR")
-        lr = LogisticRegression()
-        scores = cross_val_score(lr, X, y)
-        score = np.mean(scores)
-        logger.info("Cross validation score for LR: %.3f" % score)
-        results["neg_%f" % neg] = score
-
-        del corpus
-        del model
-        del X
-        del y
-        del lr
-
-    for k, v in results.iteritems():
-        logger.info("%s: %s" % (k, v))
+    logger.info("Training LR")
+    lr = LogisticRegression()
+    scores = cross_val_score(lr, X, y)
+    score = np.mean(scores)
+    logger.info("Cross validation score for LR: %.3f" % score)
 
 
 if __name__ == "__main__":
@@ -116,12 +100,16 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        "doc_path",
+        help="document instances file path used for training doc2vec"
+    )
+    parser.add_argument(
         "pos_path",
-        help="positive instances file path used for validation"
+        help="positive instances file path "
     )
     parser.add_argument(
         "neg_path",
-        help="negative instances file path used for validation"
+        help="negative instances file path"
     )
     args = parser.parse_args()
 
@@ -129,4 +117,4 @@ if __name__ == "__main__":
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         level=logging.INFO
     )
-    grid_search(args.pos_path, args.neg_path)
+    grid_search(args.doc_path, args.pos_path, args.neg_path)

50e0a2ebad79fa1a932472df8695ce758d01fe20
apir8181 <kazenoyumechen@gmail.com>
2016-06-20 20:40:37 +0800

    add more analysis

---
+++ rumor_analysis/rumor_fans_distribution.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+# author: Qiaoan Chen <kazenoyumechen@gmail.com>
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+
+if __name__ == "__main__":
+    """
+    Analzing the fans number distribution of rumors.
+    """
+    parser = ArgumentParser()
+    parser.add_argument(
+        "rumor_path",
+        help="rumor csv file path"
+    )
+    parser.add_argument(
+        "doc_path",
+        help="doc csv file path"
+    )
+    args = parser.parse_args()
+
+    rumor_df = pd.read_csv(args.rumor_path)
+    doc_df = pd.read_csv(args.doc_path)
+    rumor_ffans = rumor_df["ffans_avg_num"]
+    doc_ffans = doc_df["ffans_avg_num"]
+
+    print "mean fans: rumor %.3f, doc %.3f" % (rumor_ffans.mean(), doc_ffans.mean())
+    print "std fans: rumor %.3f, doc %.3f" % (rumor_ffans.std(), doc_ffans.std())
+
+    # plot hist
+    plt.style.use('ggplot')
+    plt.hist([rumor_ffans, doc_ffans], 20, histtype='bar', range=(1, 1e5), normed=1, label=['rumor', 'doc'])
+    plt.legend()
+    plt.xlabel("ffans average number")
+    plt.ylabel("Probability")
+    plt.title("Distribution of Rumor and Doc Fans Number")
+    plt.show()
+
--- rumor_analysis/rumor_text_len_distribution.py
+++ rumor_analysis/rumor_text_len_distribution.py
@@ -25,16 +25,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
     rumor_corpus = TextSegmentedCorpus(args.rumor_path)
     doc_corpus = TextSegmentedCorpus(args.doc_path)
-    rumor_lens = [len(sentence) for sentence in rumor_corpus]
-    doc_lens = [len(sentence) for sentence in doc_corpus]
+    rumor_lens = [len(''.join(sentence)) for sentence in rumor_corpus]
+    doc_lens = [len(''.join(sentence)) for sentence in doc_corpus]
 
     print "mean length, (rumor: %f, doc: %f)" % \
           (np.mean(rumor_lens), np.mean(doc_lens))
+    print "std length, (rumor: %f, doc: %f)" % \
+          (np.std(rumor_lens), np.std(doc_lens))
 
     # plot hist
     plt.style.use('ggplot')
-    plt.hist([rumor_lens, doc_lens], 15, histtype='bar', range=(0, 1500),
-             normed=1, color=['red', 'green'], label=['rumor', 'doc'])
+    plt.hist([rumor_lens, doc_lens], 15, histtype='bar', range=(1, 2000), normed=1, label=['rumor', 'doc'])
     plt.legend()
     plt.xlabel("text length")
     plt.ylabel("Probability")

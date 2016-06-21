84f45a688c09d8299f36b7761f8e5f24c71a4b72
apir8181 <kazenoyumechen@gmail.com>
2016-06-21 15:36:58 +0800

    add doc2vec result

--- README.md
+++ README.md
@@ -12,9 +12,27 @@ __Middle-sized(Text-M)__:
 
 __Large scale(Text-L)__:
 
+## Experimental Protocols
+
+__TFIDF n gram__: This bag-of-words model is constructed by selecting 500,000
+most frequent words (up to 2-gram) from the training dataset.
+We use the word count as term-frequency. The inverse document frequency is the
+logarithm of the division between total number of documents and number of
+documents where the word appears. To deal with 0-value idf, idf value is
+smoothing by adding 1.
+
+__doc2vec__: This model is implemented in gensim, and is equivalent with
+_paragraph vector_. To better understand doc2vec, we test PV-DBOW and PV-DM
+ (variants of paragraph vector) performance. The dimension of embedding is
+ fixed to 400 and other hyper parameters are selected by cross validation.
+ After getting embedding vectors, a logistic regression is applied to perform
+ classification.
+
 ## Result
 | Method  | Text-S | Text-M | Text-L |
 |---------|--------|--------|--------|
 | TFIDF 2-gram | 93.18 | | | |
-| doc2vec (from scratch) | 78.26 | | | |
+| PV-DBOW(doc2vec) | 88.3 | | | |
+| PV-DM(doc2vec) | 81.2 | | | |
+| PV(doc2vec) | 88.7 | | | |
 
--- rumor_detect/document_modelling/doc2vec/run.py
+++ rumor_detect/document_modelling/doc2vec/run.py
@@ -5,6 +5,7 @@ import argparse
 import random
 import numpy as np
 import logging
+import sklearn.utils
 from sklearn.cross_validation import cross_val_score
 from sklearn.linear_model import LogisticRegression
 from rumor_detect.document_modelling.preprocessing.corpus \
@@ -39,58 +40,66 @@ class HelperCorpus:
     def count(self):
         return self.count_
 
+    
+class CombinedCorpus:
+    def __init__(self):
+        self.corpus_list_ = []
 
-class MemoryHelperCorpus:
-    def __init__(self, pos_path, neg_path):
-        pos_corpus = HelperCorpus(pos_path, "POS")
-        neg_corpus = HelperCorpus(neg_path, "NEG")
-        self.sentences_ = [sentence for sentence in pos_corpus]
-        self.sentences_.extend([sentence for sentence in neg_corpus])
-        self.pos_count_ = pos_corpus.count()
-        self.neg_count_ = neg_corpus.count()
+    def add(self, corpus):
+        self.corpus_list_.append(corpus)
+
+    def __iter__(self):
+        for corpus in self.corpus_list_:
+            for sentence in corpus:
+                yield sentence
+
+                
+class MemoryShuffleCorpus:
+    def __init__(self, base_corpus):
+        sentences = []
+        for sentence in base_corpus:
+            sentences.append(sentence)
+        self.sentences_ = sentences
 
     def __iter__(self):
         random.shuffle(self.sentences_)
         for sentence in self.sentences_:
             yield sentence
 
-    def pos_count(self):
-        return self.pos_count_
-
-    def neg_count(self):
-        return self.neg_count_
-
-
-def grid_search(doc_path, pos_path, neg_path):
+            
+def grid_search(pos_path, neg_path):
     logger = logging.getLogger("doc2vec.run")
 
     # pretrain doc from a large dataset
-    doc_corpus = HelperCorpus(doc_path, "DOC")
-    model = Doc2Vec(doc_corpus, size=100, alpha=0.025, window=4, min_count=5,
-                    sample=0, hs=0, negative=5, dm_concat=1, iter=5, workers=7)
+    pos_corpus = HelperCorpus(pos_path, "POS")
+    neg_corpus = HelperCorpus(neg_path, "NEG")
+    combined_corpus = CombinedCorpus()
+    combined_corpus.add(pos_corpus)
+    combined_corpus.add(neg_corpus)
+    corpus = MemoryShuffleCorpus(combined_corpus)
+    # model_dm = Doc2Vec(corpus, size=400, alpha=0.025, window=4,
+    #                    min_count=5, sample=1e-5, hs=0, negative=15,
+    #                    dm=1, dm_concat=0, dbow_words=0, iter=20, workers=7)
+    model_dbow = Doc2Vec(corpus, size=400, alpha=0.025, window=4,
+                         min_count=5, sample=1e-5, hs=0, negative=15,
+                         dm=0, dm_concat=0, dbow_words=0, iter=20, workers=7)
 
-
-    # fix random seed to remove perturbation
-    logger.info("Feeding corpus into memory")
-    corpus = MemoryHelperCorpus(pos_path, neg_path)
-    logger.info("Training Doc2Vec")
-    model.train(corpus)
-
-    logger.info("Retrieving Doc2Vec")
-    X = np.zeros((corpus.pos_count() + corpus.neg_count(), 100))
-    y = np.zeros(corpus.pos_count() + corpus.neg_count(), dtype=np.int32)
-    for pos_i in range(corpus.pos_count()):
-        X[pos_i] = model.docvecs['POS_%d' % pos_i]
+    X = np.zeros((pos_corpus.count() + neg_corpus.count(), 400))
+    y = np.zeros(pos_corpus.count() + neg_corpus.count(), dtype=np.int32)
+    for pos_i in range(pos_corpus.count()):
+        X[pos_i] = model_dbow.docvecs['POS_%d' % pos_i]
         y[pos_i] = 1
-    for neg_i in range(corpus.neg_count()):
-        X[corpus.pos_count() + neg_i] = model.docvecs['NEG_%d' % neg_i]
-        y[neg_i] = -1
+    for neg_i in range(neg_corpus.count()):
+        X[pos_corpus.count() + neg_i] = model_dbow.docvecs['NEG_%d' % neg_i]
+        y[pos_corpus.count() + neg_i] = -1
+    X, y = sklearn.utils.shuffle(X, y)
 
-    logger.info("Training LR")
-    lr = LogisticRegression()
-    scores = cross_val_score(lr, X, y)
-    score = np.mean(scores)
-    logger.info("Cross validation score for LR: %.3f" % score)
+    for C in [100, 10, 1, 0.1, 0.01, 1e-3, 1e-4, 1e-5]:
+        logger.info("Training LR")
+        lr = LogisticRegression(C=C)
+        scores = cross_val_score(lr, X, y)
+        score = np.mean(scores)
+        logger.info("Cross validation score for LR (C=%f): %.3f" % (C, score))
 
 
 if __name__ == "__main__":
@@ -99,10 +108,6 @@ if __name__ == "__main__":
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "doc_path",
-        help="document instances file path used for training doc2vec"
-    )
     parser.add_argument(
         "pos_path",
         help="positive instances file path "
@@ -117,4 +122,4 @@ if __name__ == "__main__":
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         level=logging.INFO
     )
-    grid_search(args.doc_path, args.pos_path, args.neg_path)
+    grid_search(args.pos_path, args.neg_path)

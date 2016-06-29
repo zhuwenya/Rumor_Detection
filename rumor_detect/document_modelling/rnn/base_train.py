e68eb9a3d5b1dfcf348b5d28e1360b993c5567f2
apir8181 <kazenoyumechen@gmail.com>
2016-06-29 16:46:12 +0800

    rnn add gradient clip

--- rumor_detect/document_modelling/rnn/base_train.py
+++ rumor_detect/document_modelling/rnn/base_train.py
@@ -3,6 +3,9 @@
 
 import tensorflow as tf
 
+MOMENTUM = 0.9
+MOVING_AVG_RATE = 0.999
+MAX_GRADIENT_NORM = 5
 
 def train(loss, global_step, decay_steps,
           initial_learning_rate=0.01, decay_rate=0.1):
@@ -17,20 +20,22 @@ def train(loss, global_step, decay_steps,
     tf.scalar_summary('learning rate', lr)
 
     # compute and apply gradients
-    opt = tf.train.MomentumOptimizer(lr, 0.9)
-    grads = opt.compute_gradients(loss)
-    apply_grads = opt.apply_gradients(grads, global_step)
+    opt = tf.train.MomentumOptimizer(lr, MOMENTUM)
+    gradvars = opt.compute_gradients(loss)
+    clip_gradvars = [(tf.clip_by_norm(gv[0], MAX_GRADIENT_NORM), gv[1])
+                     for gv in gradvars]
+    apply_grads = opt.apply_gradients(clip_gradvars, global_step)
 
     # add histograms for variables and gradients
     for var in tf.trainable_variables():
         tf.histogram_summary(var.op.name, var)
 
-    for grad, var in grads:
+    for grad, var in clip_gradvars:
         if grad is not None:
             tf.histogram_summary(var.op.name + '/gradient', grad)
 
     # smoothing loss and variables
-    ema = tf.train.ExponentialMovingAverage(0.999, global_step)
+    ema = tf.train.ExponentialMovingAverage(MOVING_AVG_RATE, global_step)
     ema_op = ema.apply(tf.trainable_variables() + [loss])
     tf.scalar_summary('moving_loss', ema.average(loss))
 
